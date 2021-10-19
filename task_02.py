1086f7a3b2e8a069c4b56184ae7db155918601c5
Vlad Bloshenko <71842735+blotenko@users.noreply.github.com>
2021-10-19 14:18:40 +0300

    Add files via upload
    task_02

---
+++ task_02.py
@@ -0,0 +1,22 @@
+
+
+
+def int_Num_X2(num):
+    num = int(num)
+    print(num *2)
+#########################
+def float_Num_X2(num):
+    num = float(num)
+    print(num * 2)
+######################
+def str_Num_X2(s):
+    s = str(s)
+    print(s * 2)
+
+
+if __name__ == '__main__':
+    int_Num_X2(2)
+    float_Num_X2(2.2)
+    str_Num_X2("22")
+
+
